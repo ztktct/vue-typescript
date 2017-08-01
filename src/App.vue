e3fb482a5bf735a2fcce5c9e97425c7c0e507b4d
zhangtong <zhangtong@dianwoba.com>
2017-08-01 17:22:34 +0800

    Add Sass support

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import './styles/app';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
